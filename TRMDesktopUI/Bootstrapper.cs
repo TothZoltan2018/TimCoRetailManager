492a083411fdd8ff2290e96e01a6b8af4cf62fa9
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-12-16 22:05:48 +0100

    Caliburn Micro's Simple Container Dependency Injection System added.

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -18,15 +18,54 @@ namespace TRMDesktopUI
 
     class Bootstrapper : BootstrapperBase
     {
+        // This is a kind of Dependency Injection tool, like Autofac, but CaliburnMicro involve it.
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize(); // Initialize the framework
+        }
+
+        protected override void Configure()
+        {
+            // Whenever we ask for a SimpleContainer instance, it will return it.
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                // If the ShellWiewModel aks for an IEventAggregator, it gives back the first EventAggregator ever created
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            // Connect all of our ViewModels to our Views
+            // Reflection: Although this is resource consuming, but it happens only once, at startup.
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             // On startup launch ShellViewModel as our base view. Then the viewmodel will launch the view
             DisplayRootViewFor<ShellViewModel>(); 
+        }
+
+        // This creates the instance of type "service" with name "key"
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
